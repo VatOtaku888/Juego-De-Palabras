d17092146ab96c7d21b3088783bd86a66489fb44
VatOtaku888 <65376741+VatOtaku888@users.noreply.github.com>
2021-06-03 15:10:45 -0500

    Correción de errores
    Cambie algunas preguntas son las mismas pero de otra manera explicada

--- Code.py
+++ Code.py
@@ -23,7 +23,7 @@ print(Fore.GREEN + '''
     Asi que preparate!!
       
       ''')
-Value = str(input(Fore.YELLOW + "\nOption:")) #input para elegir si quieres empezar el juego o no
+Value = str(input("\nOption:")) #input para elegir si quieres empezar el juego o no
 
 
 
@@ -37,7 +37,7 @@ if Value == "Start" or Value == "start" or Value == "START": #Opcion para comenz
     ¿Cuanto es 4 + 4?
 
           ''')
-    Respuesta = int(input(Fore.YELLOW + "\nQuestion:")) #Input para responder la respuesta
+    Respuesta = int(input(f"\nQuestion:")) #Input para responder la respuesta
 #....PRIMERA PREGUNTA...#
     if Respuesta == 8: #Condicional de la primera pregunta
         print(Fore.GREEN + f'''
@@ -46,16 +46,16 @@ if Value == "Start" or Value == "start" or Value == "START": #Opcion para comenz
     .......SEGUNDA PREGUNTA.........
     ¿Cuanto es 6 x 6?
               ''')
-        Respuesta2 = int(input(Fore.YELLOW + "\nQuestion2:")) # input Para responder la pregunta2
+        Respuesta2 = int(input(f"\nQuestion2:")) # input Para responder la pregunta2
 #.....SEGUNDA PREGUNTA..#
         if Respuesta2 == 36: # Condicional De la pregunta2
             print(Fore.GREEN + f'''
     Wow wow correcto es {Respuesta2}
 
     ........TERCERA PREGUNTA.......
-    ¿a cuantos años puedes para entrar a discord?
+    ¿Que edad minima necesitas para poder entrar a discord?
                   ''')
-            Respuesta3 = int(input(Fore.YELLOW + "\nQuestion3:")) #Input de la pregunta
+            Respuesta3 = int(input(f"\nQuestion3:")) #Input de la pregunta
 #......TERCERA PREGUNTA.#
             if Respuesta3 == 13: # condicional de la pregunta
                 print(Fore.GREEN + f'''
@@ -64,7 +64,7 @@ if Value == "Start" or Value == "start" or Value == "START": #Opcion para comenz
     ........Ultima Pregunta........
     ¿Quien es el creador de facebook?
                      ''')
-                Respuesta4 = str(input(Fore.YELLOW + "\nQuestion4:"))
+                Respuesta4 = str(input(f"\nQuestion4:"))
 #.....ULTIMA PREGUNTA..#
                 if Respuesta4 == "Mark Zuckerberg" or Respuesta4 == "mark zuckerberg" or Respuesta4 == "zuckerberg" or Respuesta4 == "Mark" or Respuesta4 == "MARK ZUCKERBERG" or Respuesta4 == "mark" or Respuesta4 == "MARK": #lista de respuestas validas
                     print(Fore.GREEN + f'''
@@ -74,14 +74,14 @@ if Value == "Start" or Value == "start" or Value == "START": #Opcion para comenz
     Bonus.... End Or Bonus?
                           ''')
 #......BONUS............#
-                    Bonus_1 = str(input(Fore.YELLOW + "\nBonus:")) #input del bonus
+                    Bonus_1 = str(input(f"\nBonus:")) #input del bonus
                     if Bonus_1 == "Bonus" or Bonus_1 == "bonus" or Bonus_1 == "BONUS": #Bonus Condicional
                     
                         print(Fore.GREEN + '''
     ........BONUS QUESTION....
     ¿Eri gei? Sis Non
                         ''')
-                        SisONon = str(input(Fore.YELLOW + "\nSisNon:")) # Opciones
+                        SisONon = str(input(f"\nSisNon:")) # Opciones
                         if SisONon == "Sis" or SisONon == "sis" or SisONon == "SIS": #Condicional Opciones
                               print(Fore.RED + '''
     JAJAJAJA ERI GEI WEON
